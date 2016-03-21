aec4820b0d42e61392aea23114b6848d27ccef2f
Brian Hsu <brianhsu.hsu@gmail.com>
2016-03-21 13:10:14 +0800

    Initial commit.

---
+++ Inc/mxconstants.h
@@ -0,0 +1,77 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define TB1_LCR_Pin GPIO_PIN_12
+#define TB1_LCR_GPIO_Port GPIOB
+#define TB2_LCR_Pin GPIO_PIN_13
+#define TB2_LCR_GPIO_Port GPIOB
+#define TB1_DETECT_Pin GPIO_PIN_14
+#define TB1_DETECT_GPIO_Port GPIOB
+#define TB2_DETECT_Pin GPIO_PIN_15
+#define TB2_DETECT_GPIO_Port GPIOB
+#define TB1_HV_Pin GPIO_PIN_8
+#define TB1_HV_GPIO_Port GPIOD
+#define TB2_HV_Pin GPIO_PIN_9
+#define TB2_HV_GPIO_Port GPIOD
+#define TB1_15V_Pin GPIO_PIN_10
+#define TB1_15V_GPIO_Port GPIOD
+#define TB2_15V_Pin GPIO_PIN_11
+#define TB2_15V_GPIO_Port GPIOD
+#define TB1_LC_Pin GPIO_PIN_12
+#define TB1_LC_GPIO_Port GPIOD
+#define TB2_LC_Pin GPIO_PIN_13
+#define TB2_LC_GPIO_Port GPIOD
+#define ADC_CLK_Pin GPIO_PIN_4
+#define ADC_CLK_GPIO_Port GPIOB
+#define ADC_DATAIN_Pin GPIO_PIN_5
+#define ADC_DATAIN_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
